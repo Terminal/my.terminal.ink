406e3a8abc384c56fb7bee6ddf6518dba5d3b5eb
natanbc <natanbc@users.noreply.github.com>
2018-09-30 23:04:59 -0300

    Add configuration options

--- src/main/java/ink/terminal/my/Application.java
+++ src/main/java/ink/terminal/my/Application.java
@@ -6,7 +6,17 @@ import static spark.Spark.*;
 
 public  class Application {
     public static void main(String[] args) {
-        port(8080);
+        port(Integer.getInteger("app.port", 8080));
+        threadPool(Integer.getInteger("app.pool.max", -1), Integer.getInteger("app.pool.min", -1),
+                Integer.getInteger("app.pool.idle_timeout", -1));
+        String staticFileLocation = System.getProperty("app.static_file_location");
+        if(staticFileLocation != null) {
+            staticFileLocation(staticFileLocation);
+        }
+        String externalStaticFileLocation = System.getProperty("app.external_static_file_location");
+        if(externalStaticFileLocation != null) {
+            externalStaticFileLocation(externalStaticFileLocation);
+        }
 
         DBHelper db = new DBHelper();
     }
